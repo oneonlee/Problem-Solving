bd1f099d2e74deb9dabfc6b732564921172b410e
Dong-geon Lee <peter8739@gmail.com>
2022-06-11 23:46:53 +0900

    2022. 06. 11. 1트

---
+++ Baekjoon OJ/1254. 팰린드롬 만들기.py
@@ -0,0 +1,16 @@
+S = input()
+
+idx = len(S)-1
+
+i = 0
+while i<len(S)-1:
+
+  if S[i] == S[idx]:
+    idx = idx - 1
+  elif idx != len(S)-1:
+    idx = len(S)-1
+    if S[i] == S[idx]:
+      idx = idx - 1
+  i = i+1
+
+print(i + idx + 1)
