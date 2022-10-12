a4f5ba571e2097a3c45089284ba8edd5dc569582
Dong-geon Lee <peter8739@gmail.com>
2022-10-12 23:17:12 +0900

    2022. 10. 12. 푸는중

--- Baekjoon OJ/1061. 제곱 ㄴㄴ 수.py
+++ Baekjoon OJ/1061. 제곱 ㄴㄴ 수.py
@@ -4,7 +4,7 @@ def foo(N):
     if N<4:
         return True
         
-    sqrt = int(math.sqrt(N))
+    sqrt = int(math.sqrt(N)) 
     for num in range(2, sqrt+1):
         if N%math.pow(num, 2) == 0:
             # print(N, math.pow(num, 2))
@@ -15,7 +15,13 @@ a, b = input().split()
 a = int(a)
 b = int(b)
 
+
+square_number = [int(math.pow(num, 2)) for num in range(int(math.sqrt(a)), int(math.sqrt(b)) + 1)]
+
+print(square_number)
 cnt = 0
+
+
 for i in range(a, b+1):
     if foo(i)==True:
         # print(i)
