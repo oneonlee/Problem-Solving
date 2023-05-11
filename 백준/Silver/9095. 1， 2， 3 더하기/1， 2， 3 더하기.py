e4005bcbba08e7248be80b9cb9b610b799b5bca3
Dong-geon Lee <peter8739@gmail.com>
2023-05-11 21:40:38 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,15 +1,26 @@
-def foo(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 2
-    elif n==3:
-        return 4
-    else:
-        return foo(n-1) + foo(n-2) + foo(n-3)
+def making_resukt_list(L, max_num):
+    if max_num>3:
+        for n in range(4, max_num+1):
+            L[n] = L[n-1] + L[n-2] + L[n-3]
+    return L
+    
+L = [False for _ in range(11)]
+
+L[1] = 1
+L[2] = 2
+L[3] = 4
+
+
     
 num = int(input())
 
+in_list = []
+
 for _ in range(num):
     n = int(input())
-    print(foo(n))
+    in_list.append(n)
+    
+making_resukt_list(L, max(in_list))
+
+for n in in_list:
+    print(L[n])
