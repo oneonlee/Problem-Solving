389adc583389350fdc2e542d7f5875bfca7ae747
Dong-geon Lee <peter8739@gmail.com>
2023-08-20 21:24:12 +0900

    [level 2] Title: 전화번호 목록, Time: 97.45 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    phone_book.sort()
+    
+    for idx in range(len(phone_book) - 1):
+        if phone_book[idx] == phone_book[idx+1][:len(phone_book[idx])]:
+            return False
+                
+    return True
